5fd7cccbe47abc0a927340d44cd3838703fdc40c
Adrian Kögl <aak2229@columbia.edu>
2024-02-14 13:59:12 +0100

    fine tuning

--- js/index.js
+++ js/index.js
@@ -2,47 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("dark-mode-toggle");
   const bodyElement = document.body;
 
-  /**var gridContainer = document.querySelector('.grid-container');
-  var originalContent = gridContainer.innerHTML;
-  // Duplicate content multiple times to extend the seamless period
-  for (let i = 0; i < 5; i++) {
-    gridContainer.innerHTML += originalContent;
-  }**/
-
-  /*const slider = document.querySelector('.grid-container');
-  let isHovering = false;
-  let speed = 1; // Speed of the slider movement
-
-  function moveSlider() {
-    if (!isHovering) {
-      // Move the slider
-      slider.scrollLeft += speed;
-
-      // Check if the first element is fully out of view
-      let firstElement = slider.querySelector('.grid-item');
-      if (firstElement.getBoundingClientRect().right < 0) {
-        // Move the first element to the end
-        slider.appendChild(firstElement);
-        slider.scrollLeft -= firstElement.offsetWidth; // Adjust scroll position after moving the first element
-      }
-    }
-  }*/
-
-  // Update the slider every 20ms for smooth movement
-  setInterval(moveSlider, 20);
-
-  // Pause on hover
-  slider.addEventListener('mouseenter', () => isHovering = true);
-  slider.addEventListener('mouseleave', () => isHovering = false);
-
-  // Set the interval for how often logos are shifted
-  const shiftSpeed = 2000; // Adjust based on desired speed (in milliseconds)
-  setInterval(shiftLogo, shiftSpeed);
-
-  // Pause shifting on hover
-  slider.addEventListener('mouseenter', () => isHovering = true);
-  slider.addEventListener('mouseleave', () => isHovering = false);
-
   toggleButton.addEventListener("click", function () {
     bodyElement.classList.toggle("latex-dark");
 
